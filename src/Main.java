71cc6dbd6acecd3bc72223abe45fe33bd265474f
DealDOne <vladijsaf@mail.ru>
2024-01-09 22:42:11 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,58 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Main {
+
+    public static Runnable maRunnable(BlockingQueue<String> textsForA, BlockingQueue<FutureTask<Integer>> futureTaskA, char a, int length, int amountOfStrings, int capacityForQueue) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                for (int tr = 0; tr < amountOfStrings; tr++) {
+                    String someText;
+                    try {
+                        someText = textsForA.take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    int someTextLength = someText.length();
+                    Callable<Integer> myCallableForMaxSizeA = () -> {
+                        int maxSizeA = 0;
+                        for (int i = 0; i < someTextLength; i++) {
+                            for (int j = 0; j < someTextLength; j++) {
+                                if (i >= j) {
+                                    continue;
+                                }
+                                boolean aFound = false;
+                                for (int k = i; k < j; k++) {
+                                    if (someText.charAt(k) == a) {
+                                        aFound = true;
+                                        break;
+                                    }
+                                }
+                                if (!aFound && maxSizeA < j - i) {
+                                    maxSizeA = j - i;
+                                }
+                            }
+                        }
+                        return maxSizeA;
+                    };
+                    FutureTask<Integer> someFutureTaskA = new FutureTask<>(myCallableForMaxSizeA);
+                    try {
+                        futureTaskA.put(someFutureTaskA);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Thread thread = new Thread(someFutureTaskA);
+                    thread.start();
+                }
+            }
+        };
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        final int length = 100_000;
-        final int amountOfStrings = 30_000;
-        final int capacityForQueue = 100;
+        final int length = 30_000;
+        final int amountOfStrings = 25;
+        final int capacityForQueue = 10;
 
         long startThreads = System.currentTimeMillis();
 
@@ -43,131 +90,9 @@ public class Main {
 
         thread1.start();
 
-        Runnable myRunnableA = () -> {
-            for (int tr = 0; tr < amountOfStrings; tr++) {
-                String someText;
-                try {
-                    someText = textsForA.take();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                int someTextLength = someText.length();
-                Callable<Integer> myCallableForMaxSizeA = () -> {
-                    int maxSizeA = 0;
-                    for (int i = 0; i < someTextLength; i++) {
-                        for (int j = 0; j < someTextLength; j++) {
-                            if (i >= j) {
-                                continue;
-                            }
-                            boolean aFound = false;
-                            for (int k = i; k < j; k++) {
-                                if (someText.charAt(k) == 'a') {
-                                    aFound = true;
-                                    break;
-                                }
-                            }
-                            if (!aFound && maxSizeA < j - i) {
-                                maxSizeA = j - i;
-                            }
-                        }
-                    }
-                    return maxSizeA;
-                };
-                FutureTask<Integer> someFutureTaskA = new FutureTask<>(myCallableForMaxSizeA);
-                try {
-                    futureTaskA.put(someFutureTaskA);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                Thread thread = new Thread(someFutureTaskA);
-                thread.start();
-            }
-        };
-
-        Runnable myRunnableB = () -> {
-            for (int tr = 0; tr < amountOfStrings; tr++) {
-                String someText;
-                try {
-                    someText = textsForB.take();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                int someTextLength = someText.length();
-                Callable<Integer> myCallableForMaxSizeB = () -> {
-                    int maxSizeB = 0;
-                    for (int i = 0; i < someTextLength; i++) {
-                        for (int j = 0; j < someTextLength; j++) {
-                            if (i >= j) {
-                                continue;
-                            }
-                            boolean bFound = false;
-                            for (int k = i; k < j; k++) {
-                                if (someText.charAt(k) == 'b') {
-                                    bFound = true;
-                                    break;
-                                }
-                            }
-                            if (!bFound && maxSizeB < j - i) {
-                                maxSizeB = j - i;
-                            }
-                        }
-                    }
-                    return maxSizeB;
-                };
-                FutureTask<Integer> someFutureTaskB = new FutureTask<>(myCallableForMaxSizeB);
-                try {
-                    futureTaskB.put(someFutureTaskB);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                Thread thread = new Thread(someFutureTaskB);
-                thread.start();
-            }
-        };
-
-        Runnable myRunnableC = () -> {
-            for (int tr = 0; tr < amountOfStrings; tr++) {
-                String someText;
-                try {
-                    someText = textsForC.take();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                int someTextLength = someText.length();
-                Callable<Integer> myCallableForMaxSizeC = () -> {
-                    int maxSizeC = 0;
-                    for (int i = 0; i < someTextLength; i++) {
-                        for (int j = 0; j < someTextLength; j++) {
-                            if (i >= j) {
-                                continue;
-                            }
-                            boolean cFound = false;
-                            for (int k = i; k < j; k++) {
-                                if (someText.charAt(k) == 'c') {
-                                    cFound = true;
-                                    break;
-                                }
-                            }
-                            if (!cFound && maxSizeC < j - i) {
-                                maxSizeC = j - i;
-                            }
-                        }
-                    }
-                    return maxSizeC;
-                };
-                FutureTask<Integer> someFutureTaskC = new FutureTask<>(myCallableForMaxSizeC);
-                try {
-                    futureTaskC.put(someFutureTaskC);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                Thread thread = new Thread(someFutureTaskC);
-                thread.start();
-            }
-        };
-        Thread threadA = new Thread(myRunnableA);
-        Thread threadB = new Thread(myRunnableB);
-        Thread threadC = new Thread(myRunnableC);
+        Thread threadA = new Thread(maRunnable(textsForA, futureTaskA, 'a', length, amountOfStrings, capacityForQueue));
+        Thread threadB = new Thread(maRunnable(textsForB, futureTaskB, 'b', length, amountOfStrings, capacityForQueue));
+        Thread threadC = new Thread(maRunnable(textsForC, futureTaskC, 'c', length, amountOfStrings, capacityForQueue));
 
         threadA.start();
         threadB.start();
@@ -183,41 +108,42 @@ public class Main {
                     int currentValueAFromQueue = futureTaskA.take().get();
                     if (currentValueAFromQueue > MaxA.get()) {
                         MaxA.getAndSet(currentValueAFromQueue);
-                    };
-                    if (tr == amountOfStrings -1) {
+                    }
+                    ;
+                    if (tr == amountOfStrings - 1) {
                         long endThreads = System.currentTimeMillis();
                         System.out.println("maxSize case 'a' -> " + MaxA.get());
                         System.out.println("Time: " + (endThreads - startThreads) + "ms");
                     }
-                }catch (ExecutionException | InterruptedException e) {
+                } catch (ExecutionException | InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 try {
                     int currentValueBFromQueue = futureTaskB.take().get();
                     if (currentValueBFromQueue > MaxB.get()) {
                         MaxB.getAndSet(currentValueBFromQueue);
-                    };
-                    if (tr == amountOfStrings -1) {
+                    }
+                    ;
+                    if (tr == amountOfStrings - 1) {
                         long endThreads = System.currentTimeMillis();
                         System.out.println("maxSize case 'b' -> " + MaxB.get());
                         System.out.println("Time: " + (endThreads - startThreads) + "ms");
                     }
-                }catch (ExecutionException | InterruptedException e) {
+                } catch (ExecutionException | InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 try {
                     int currentValueCFromQueue = futureTaskC.take().get();
                     if (currentValueCFromQueue > MaxC.get()) {
                         MaxC.getAndSet(currentValueCFromQueue);
-                    };
-                    if (tr == amountOfStrings -1) {
+                    }
+                    ;
+                    if (tr == amountOfStrings - 1) {
                         long endThreads = System.currentTimeMillis();
                         System.out.println("maxSize case 'c' -> " + MaxC.get());
                         System.out.println("Time: " + (endThreads - startThreads) + "ms");
                     }
-                }
-
-                catch (ExecutionException | InterruptedException e) {
+                } catch (ExecutionException | InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
